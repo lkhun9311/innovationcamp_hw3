1fb7ece1a1a780c10c65ceb8a7bc0ff03e351b36
kwanghunlee <lkhun9311@naver.com>
2022-08-03 18:14:27 +0900

    Add BoardCommentRepository 생성, JpaRepository 상속

---
+++ src/main/java/com/sparta/homework3/repository/BoardCommentRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.homework3.repository;
+
+import com.sparta.homework3.domain.BoardComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardCommentRepository extends JpaRepository<BoardComment, Long> {
+    List<BoardComment> findAllByCommentIdOrderByCreatedAtDesc(Long CommentID);
+}
