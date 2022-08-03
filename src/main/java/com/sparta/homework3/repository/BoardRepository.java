0d635b4d3f35364a18e6b03338744febab5a011b
kwanghunlee <lkhun9311@naver.com>
2022-08-03 18:10:28 +0900

    Add BoardRepository 생성, JpaRepository 상속

---
+++ src/main/java/com/sparta/homework3/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.homework3.repository;
+
+import com.sparta.homework3.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findAllByOrderByCreatedAtDesc();
+}
