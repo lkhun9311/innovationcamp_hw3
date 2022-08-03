0d82825ccbb78437a0716fe110051ef8ffc86d0c
kwanghunlee <lkhun9311@naver.com>
2022-08-03 22:31:21 +0900

    Fix Homework3Application의 에노테이션 수정

--- src/main/java/com/sparta/homework3/Homework3Application.java
+++ src/main/java/com/sparta/homework3/Homework3Application.java
@@ -2,11 +2,10 @@ package com.sparta.homework3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
+@ConfigurationPropertiesScan
 @EnableJpaAuditing
 @SpringBootApplication
 public class Homework3Application {
@@ -14,10 +13,4 @@ public class Homework3Application {
     public static void main(String[] args) {
         SpringApplication.run(Homework3Application.class, args);
     }
-
-    @PostConstruct
-    public void recordTime() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
-
 }
