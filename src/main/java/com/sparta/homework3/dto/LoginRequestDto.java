b12c0a5f7ae430030ef99785dbf74e957a7ca028
kwanghunlee <lkhun9311@naver.com>
2022-08-03 22:36:31 +0900

    Add LoginRequestDto 생성

---
+++ src/main/java/com/sparta/homework3/dto/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.sparta.homework3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
