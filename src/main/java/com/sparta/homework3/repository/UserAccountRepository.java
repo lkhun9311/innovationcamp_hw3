0a5e9d58ab0073eb71765c97e42418ebf8f7eb30
kwanghunlee <lkhun9311@naver.com>
2022-08-03 18:07:12 +0900

    Add UserAccountRepository 생성, JpaRepository 상속

---
+++ src/main/java/com/sparta/homework3/repository/UserAccountRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.homework3.repository;
+
+import com.sparta.homework3.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    Optional<UserAccount> findByUsername(String username);
+}
