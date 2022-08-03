ea3031b2e9cf77333ff1ea09cd8fce6a6a217ed7
kwanghunlee <lkhun9311@naver.com>
2022-08-03 17:24:32 +0900

    Add 데이터 생성, 변경 시점을 자동으로 데이터베이스에 매핑해주기 위해 에노테이션 작성

--- src/main/java/com/sparta/homework3/Homework3Application.java
+++ src/main/java/com/sparta/homework3/Homework3Application.java
@@ -2,12 +2,22 @@ package com.sparta.homework3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class Homework3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Homework3Application.class, args);
+    }
+
+    @PostConstruct
+    public void recordTime() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
